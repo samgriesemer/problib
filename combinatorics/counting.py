daf2f38709c196ac4712873b58b2439507b57fa4
Sam Griesemer <samgriesemer@gmail.com>
2019-04-05 02:56:12 -0500

    add static perm and comb methods, complete sample_wo_replacement, docstrings

--- combinatorics/counting.py
+++ combinatorics/counting.py
@@ -7,15 +7,31 @@ class Product:
   def __init__(self, *data):
     self.data = data
 
+  def count(self):
+    '''Compute number of elements in product'''
+    count = 1
+    for datum in self.data:
+      count *= len(datum)
+    return count
+
   def generate(self):
+    '''Generate all Cartesian product set'''
     return itertools.product(*self.data)
 
   def sample(self, m=1):
+    '''Randomly generate a sample(s) from the product'''
     for _ in range(m):
       yield tuple(random.choice(exp) for exp in self.data)
 
   def sample_without_replacement(self, m=1):
-    pass
+    '''Randomly generate m unique samples from the product'''
+    if m > self.count(): return None
+    generated = []
+    while len(generated) < m:
+      gtuple = next(self.sample(1))
+      if gtuple not in generated:
+        generated.append(gtuple)
+        yield gtuple
 
 class Permutation:
   '''Permutations of iterables'''
@@ -23,16 +39,24 @@ class Permutation:
     self.data = data
     self.n = len(data)
 
+  @staticmethod
+  def nPk(n, k):
+    '''Compute nPk'''
+    return int(factorial(n) / factorial(n-k))
+
   def count(self, k=None):
+    '''Compute nPk expxlicitly on object data'''
     if k is None: k = self.n
     if k > self.n: return None
-    return int(factorial(self.n) / factorial(self.n-k))
+    return Permutation.nPk(self.n, k)
     
   def generate(self, k=None):
+    '''Return generator over all permutations of object data'''
     if k is None: k = self.n
     return itertools.permutations(self.data, k)
 
   def sample(self, k=None, m=1):
+    '''Return generator over m random samples from k-permutations of object data'''
     if k is None: k = self.n
     if k > self.n: return None
     for _ in range(m):
@@ -47,16 +71,24 @@ class Combination:
     self.data = data
     self.n = len(data)
 
+  @staticmethod
+  def nCk(n, k):
+    '''Compute nCk'''
+    return int(factorial(n) / (factorial(k)*factorial(n-k)))
+
   def count(self, k=None):
+    '''Compute nCk implicitly on object data'''
     if k is None: k = self.n
     if k > self.n: return None
-    return int(factorial(self.n) / (factorial(k)*factorial(self.n-k)))
+    return Combination.nCk(self.n, k)
     
   def generate(self, k=None):
+    '''Return generator over all combinations of object data'''
     if k is None: k = self.n
     return itertools.combinations(self.data, k)
 
   def sample(self, k=None, m=1):
+    '''Return generator over m random samples from k-combinations of object data'''
     if k is None: k = self.n
     if k > self.n: return None
     for _ in range(m):
