d65a2003c9cd9e94d7a43bf389c7f44fad3b38e1
Sam Griesemer <samgriesemer@gmail.com>
2019-09-05 11:35:04 -0500

    small leetcode update

---
+++ algorithms/leetcode.py
@@ -0,0 +1,22 @@
+# Container with most water
+# https://leetcode.com/problems/container-with-most-water/
+
+# visualization tool
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.array([1,8,6,2,5,4,8,3,7])
+x = np.arange(0,len(arr))
+y = np.arange(0,len(arr))
+X, Y = np.meshgrid(x,y)
+t = (X-Y)
+m = np.minimum(arr[X],arr[Y])
+Z = np.multiply(t,m)
+
+# contour plot
+fig, ax = plt.subplots()
+CS = ax.contour(X, Y, Z)
+ax.clabel(CS, inline=1, fontsize=10)
+fig.show()
