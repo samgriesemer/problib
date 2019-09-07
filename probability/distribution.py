f66f37ad085513062cffd48bb05a9260f32cdaf7
Sam Griesemer <samgriesemer@gmail.com>
2019-09-07 17:59:43 -0500

    add distribution class

---
+++ probability/distribution.py
@@ -0,0 +1,10 @@
+class Distribution():
+  def __init__(self):
+    pass
+
+  def sample(self, n):
+    '''n: number of samples'''
+    pass
+
+  def cdf(self, x):
+    '''return cdf(x) = Pr(x <= X)'''
