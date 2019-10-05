194ebef465d8042551592b8fd39c47410c4be88a
Sam Griesemer <samgriesemer@gmail.com>
2019-10-04 20:54:45 -0500

    slight time tweak

--- algorithms/mas.py
+++ algorithms/mas.py
@@ -34,31 +34,24 @@ def lp_auction(matrix, epsilon):
   assign_vars = np.array([[pulp.LpVariable(f'x_{i:0{len(str(i))}}_{j:0{len(str(j))}}', cat="Binary") for j in range(m)] for i in range(n)])
 
   # objective fucnction
-  #prob += np.sum(np.multiply(matrix, assign_vars))
   vals = np.multiply(matrix, assign_vars).flatten()
-  #prob += pulp.LpAffineExpression([(val,1) for val in vals])
   prob += pulp.lpSum(list(vals))
 
   # add agent constraints
   for i in range(n):
-    #prob += np.sum(assign_vars[i,:]) <= 1
-    #prob += pulp.LpAffineExpression([(val,1) for val in assign_vars[i,:]]) <= 1
     prob += pulp.lpSum(list(assign_vars[i,:])) <= 1
 
   # add object constraints
   for j in range(n):
-    #prob += np.sum(assign_vars[:,j]) <= 1
-    #prob += pulp.LpAffineExpression([(val,1) for val in assign_vars[:,j]]) <= 1
     prob += pulp.lpSum(list(assign_vars[:,j])) <= 1
 
   # solve lp problem
   prob.solve()
-  #pulp.GLPK_CMD()
-  #print(pulp.LpStatus[prob.status])
   return np.array(list(map(lambda x: x.varValue, prob.variables()))).reshape((n,m))
 
 def assignment_generator(n, M):
-  return np.random.randint(1, M, (n,n))
+  #return np.random.randint(0, M-1, (n,n))
+  return np.random.uniform(0, M-1, (n,n))
 
 def experiment(trials, pow_range, M):
   exp_avg = []
@@ -78,7 +71,7 @@ def time_experiment(trials, n, pow_range):
   auc_list = []
   lp_auc_list = []
   for i in range(*pow_range):
-    M = 10**i-20
+    M = 10**i
     auc_time = 0
     lp_auc_time = 0
     for j in tqdm(range(trials)):
@@ -90,10 +83,10 @@ def time_experiment(trials, n, pow_range):
       auc_time += (end-start)
 
       # lp auction algo
-      start = time.process_time()
-      lp_auction(prob, 1/(2*n))
-      end = time.process_time()
-      lp_auc_time += (end-start)
+      # start = time.process_time()
+      # lp_auction(prob, 1/(2*n))
+      # end = time.process_time()
+      # lp_auc_time += (end-start)
 
     auc_list.append(auc_time/trials)
     lp_auc_list.append(lp_auc_time/trials)
