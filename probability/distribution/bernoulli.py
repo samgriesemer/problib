a0fbdc29d18443b4a1a2a54c1c482c6daa3a963a
Sam Griesemer <samgriesemer@gmail.com>
2019-10-07 03:57:06 -0500

    refactor bernoulli

--- probability/distribution/bernoulli.py
+++
@@ -1,7 +0,0 @@
-class Bernoulli(Distribution):
-  def __init__(self, p):
-    self.p = p
-
-  def sample(self, n=1):
-    for _ in range(n):
-      yield 1 if random.random() < self.p else 0
--- probability/distributions/bernoulli.py
+++ probability/distributions/bernoulli.py
@@ -6,6 +6,13 @@ class Bernoulli(distribution.Distribution):
   def __init__(self, p):
     self.p = p
 
+  def pdf(self, x):
+    return self.p**x * (1-self.p)**(1-x)
+
+  def cdf(self, x):
+    return (1-self.p)**(1-int(x))
+
   def sample(self, n=1):
     for _ in range(n):
       yield 1 if random.random() < self.p else 0
+
