8e5e4962c3b295d4e3d0a6d6068f019c6d7e33fb
sam griesemer <samgriesemer@gmail.com>
2019-12-14 14:08:13 -0600

    add traffic stuffs

--- problib/braess.py
+++ problib/braess.py
@@ -1,36 +1,48 @@
-from .simulation.gym import gym, env, agent
+from .simulation.gym import gym, env, agent, policy
+
+class Car():
+    def __init__(self, aid):
+        self.aid = aid
+        self.state = [0]
+    
+    def drive(self, road):
+        self.state.append(road)
 
 class TrafficAgent(agent.Agent):
     def action(self):
         '''Decide on road to take'''
-        action = ei(self.state_history) 
+        action = policy.ei(self.state_history) 
         return action
 
-class BraessEnv(env.Env):
-    def __init__(self, superhighway=False):
-        self.superhighway = superhighway
-        if superhighway:
+class TrafficEnv(env.Env):
+    def __init__(self, superhighway=None, f=25, d=100):
             self.state = {
-                0: [1,2],
-                1: [3],
-                2: [3],
-                3: []
+                'roads': {
+                    0: [1,2],
+                    1: [3],
+                    2: [3],
+                    3: []
+                },
+                'costs': {
+                    0: [0, f],
+                    1: [f],
+                    2: [0],
+                    3: []
+                }
             }
-        else:
-            self.state = {
-                0: [1,2],
-                1: [2,3],
-                2: [3],
-                3: []
-            }
+        if superhighway is not None:
+            self.state['roads'][1].insert(0,2)
+            self.state['costs'][1].insert(0,0)
 
     def tick(self, actions):
         states = {}
+        costs = {}
         for aid, action in actions.items():
-            states[aid] = self.state[action]
+            states[aid] = self.state['roads'][action]
+            if action == 1: costs[0][0] += 1/d
         return states
 
-class BraessGym(gym.Gym):
+class TrafficGym(gym.Gym):
     def tick(self):
         action = {}
         self.gen += 1
